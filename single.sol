55f36f43a0821c4bac22034e9cc3a4bed920db0e
rakshit98 <rakshit98.goyal@gmail.com>
2020-05-13 03:27:13 +0530

    Sol v2 and adding signUp.js

--- single.sol
+++ single.sol
@@ -8,6 +8,7 @@ mapping (uint256 => uint256) _balance;
 event MoneySent(string milestone, uint256 amount);
 event Target(string milestone);
 event NGO_Transfer(string success);
+event SignUp(string message,uint256 index,string name , uint256 target, uint256 Owner);
 
 
 struct NGO {
@@ -26,17 +27,25 @@ NGO[100] private list;
 
 
 
-constructor(string memory fundraiser_name , uint256 fundraiser_target, uint256 index, uint256 owner) public {
+function sign_up(string memory fundraiser_name , uint256 fundraiser_target, uint256 index, uint256 owner) public {
 	list[index].ngo_id = index;
 	list[index].name = fundraiser_name;
 	list[index].target = fundraiser_target;
+	if(owner == 0){
 	list[index].isOwner = true;
+	}
+	else
+	{
+	    list[index].isOwner = false;
+	}
 	list[index].milestone[0] = fundraiser_target/3;
 	list[index].milestone[1] = 2*fundraiser_target/3;
 	list[index].milestone[2] = fundraiser_target;
 	list[index].owner = owner;
 	list[index].fund = 0;
 	list[index].last_thresh = 0;
+
+	emit SignUp("New Account Created",index,list[index].name,list[index].milestone[2],list[index].owner);
 }
 
 function milestone (uint256 id, uint256 amount , uint256 last) public returns(bool res, uint256 transfer_amount, uint256 owner) {
@@ -88,7 +97,9 @@ function milestone (uint256 id, uint256 amount , uint256 last) public returns(bo
 }
 
 function show_balance(uint256 fundraiser_id) public view returns(uint256){
-		//if(isOwner) check.
+		if(list[fundraiser_id].isOwner)
+		return _balance[fundraiser_id];
+
 		return list[fundraiser_id].fund;
 }
 
