9a68d7cf24c43c70f5798e450af38cb7dbfb6a24
Rishabh <rishabh.guha@oyorooms.com>
2020-05-19 00:01:32 +0530

    Added package.json and modified .gitignore

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	apiPrefix: 'https://alpha-api.ethvigil.com/v0.1/contract/',
+	apiKey: '9b7f0f18-ed7a-4c3b-8493-503235320e90',
+	//DonorContractAddress: '',
+	NGOContractAddress: '0xf7b6d33693dad35360c39111a9e7765a251cbc53',
+	wsUrl: 'wss://alpha.ethvigil.com/ws'
+}
