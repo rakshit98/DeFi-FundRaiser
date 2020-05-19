b98f91ec2a949234d511b58b5050406854f00b80
rakshit98 <rakshit98.goyal@gmail.com>
2020-05-19 19:41:40 +0530

    Final Contract

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
 	apiPrefix: 'https://alpha-api.ethvigil.com/v0.1/contract/',
 	apiKey: '9b7f0f18-ed7a-4c3b-8493-503235320e90',
 	//DonorContractAddress: '',
-	NGOContractAddress: '0xf7b6d33693dad35360c39111a9e7765a251cbc53',
+	NGOContractAddress: '0x1bb4cb4b4f45240a8fc43ee9e9cbcf3ef203a517',
 	wsUrl: 'wss://alpha.ethvigil.com/ws'
 }
