417d17d86b0dd4a1bcaabf1ddf8a8d4fa1cc1ba2
rakshit98 <rakshit98.goyal@gmail.com>
2020-05-25 13:30:13 +0530

    Removing Credentials

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	apiPrefix: 'https://alpha-api.ethvigil.com/v0.1/contract/',
-	apiKey: '9b7f0f18-ed7a-4c3b-8493-503235320e90',
-	NGOContractAddress: '0x28378eabeeacfb26c0edd07ba4f7e43b0e90d91d',
+	apiKey: '',
+	NGOContractAddress: '',
 	wsUrl: 'wss://alpha.ethvigil.com/ws'
 }
